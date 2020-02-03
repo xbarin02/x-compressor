24cfdb321b258db546ee609a0723d6aa81282037
David Barina <ibarina@fit.vutbr.cz>
2020-02-03 11:24:48 +0100

    grenc: removed non-adaptive version

--- grenc.c
+++ grenc.c
@@ -5,11 +5,6 @@
 
 #include "common.h"
 #include "bio.h"
-#include "vector.h"
-
-#if 1
-#	define ADAPTIVE
-#endif
 
 typedef unsigned char uchar;
 
@@ -77,13 +72,10 @@ retry:
 	}
 }
 
-#ifdef ADAPTIVE
 static size_t opt_k = 3;
 static size_t sum_delta = 0;
 static size_t N = 0;
-#endif
 
-#ifdef ADAPTIVE
 /* https://ipnpr.jpl.nasa.gov/progress_report/42-159/159E.pdf */
 void update_model(UINT32 delta)
 {
@@ -100,17 +92,10 @@ void update_model(UINT32 delta)
 
 	--k;
 
-	/* printf("selected k = %i\n", k); */
-
 	opt_k = k;
 }
-#endif
 
-#ifdef ADAPTIVE
 void process(FILE *istream, struct bio *bio)
-#else
-void process(FILE *istream, struct vector *vector)
-#endif
 {
 	do {
 		int c = fgetc(istream);
@@ -123,57 +108,14 @@ void process(FILE *istream, struct vector *vector)
 		assert(c < 256);
 
 		d = get_index(c);
-#ifdef ADAPTIVE
+
 		bio_write_gr(bio, opt_k, d);
-#else
-		vector_push(vector, d);
-#endif
 
 		/* update model */
 		inc_freq((uchar)c);
 
-#ifdef ADAPTIVE
 		update_model(d);
-#endif
 	} while (1);
-}
-
-size_t vector_opt_gr_k(struct vector *vector)
-{
-	size_t k;
-	size_t min_size = (size_t)-1;
-	size_t min_size_k = 0;
-
-	/* for each parameter */
-	for (k = 0; k < 20; ++k) {
-		size_t size = 0;
-		UINT32 *ptr;
-
-		for (ptr = vector->begin; ptr < vector->end; ++ptr) {
-			size += bio_sizeof_gr(k, *ptr);
-		}
-
-		if (size < min_size) {
-			min_size = size;
-			min_size_k = k;
-		}
-	}
-
-	printf("best k = %lu\n", min_size_k);
-
-	return min_size_k;
-}
-
-int vector_to_bio(struct vector *vector, struct bio *bio)
-{
-	size_t k = vector_opt_gr_k(vector);
-	UINT32 *ptr;
-
-	for (ptr = vector->begin; ptr < vector->end; ++ptr) {
-		bio_write_gr(bio, k, *ptr);
-	}
-
-	return 0;
 }
 
 void bio_dump(struct bio *bio, void *ptr, FILE *bstream)
@@ -193,9 +135,6 @@ int main(int argc, char *argv[])
 	FILE *ostream = fopen(argc > 2 ? argv[2] : "L.gr", "w");
 	struct bio bio;
 	void *ptr = malloc(100000000);
-#ifndef ADAPTIVE
-	struct vector vector;
-#endif
 
 	if (istream == NULL) {
 		abort();
@@ -211,24 +150,13 @@ int main(int argc, char *argv[])
 
 	init();
 
-#ifndef ADAPTIVE
-	vector_init(&vector, 100000000);
-#endif
-
 	bio_open(&bio, ptr, BIO_MODE_WRITE);
-#ifdef ADAPTIVE
+
 	process(istream, &bio);
 	printf("opt. k = %lu\n", (unsigned long)opt_k);
-#else
-	process(istream, &vector);
-	vector_to_bio(&vector, &bio);
-#endif
+
 	bio_close(&bio);
 	bio_dump(&bio, ptr, ostream);
-
-#ifndef ADAPTIVE
-	vector_free(&vector);
-#endif
 
 	fclose(istream);
 	fclose(ostream);
