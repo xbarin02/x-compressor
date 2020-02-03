5fc555cb514e6758cbf2023de3f8386cdaeb4af0
David Barina <ibarina@fit.vutbr.cz>
2020-02-03 11:11:12 +0100

    adaptive Golomb-Rice encoder

--- grenc.c
+++ grenc.c
@@ -7,6 +7,10 @@
 #include "bio.h"
 #include "vector.h"
 
+#if 1
+#	define ADAPTIVE
+#endif
+
 typedef unsigned char uchar;
 
 /* char -> frequency */
@@ -73,10 +77,42 @@ retry:
 	}
 }
 
+#ifdef ADAPTIVE
+static size_t opt_k = 3;
+static size_t sum_delta = 0;
+static size_t N = 0;
+#endif
+
+#ifdef ADAPTIVE
+/* https://ipnpr.jpl.nasa.gov/progress_report/42-159/159E.pdf */
+void update_model(UINT32 delta)
+{
+	int k;
+
+	sum_delta += delta;
+	N++;
+
+	/* mean = E{delta} = sum_delta / N */
+
+	/* 2^k <= E{r[k]} + 0 */
+	for (k = 0; (N << k) <= sum_delta; ++k)
+		;
+
+	/* printf("selected k = %i\n", k); */
+
+	opt_k = k;
+}
+#endif
+
+#ifdef ADAPTIVE
+void process(FILE *istream, struct bio *bio)
+#else
 void process(FILE *istream, struct vector *vector)
+#endif
 {
 	do {
 		int c = fgetc(istream);
+		UINT32 d;
 
 		if (c == EOF) {
 			break;
@@ -84,10 +120,19 @@ void process(FILE *istream, struct vector *vector)
 
 		assert(c < 256);
 
-		vector_push(vector, get_index(c));
+		d = get_index(c);
+#ifdef ADAPTIVE
+		bio_write_gr(bio, opt_k, d);
+#else
+		vector_push(vector, d);
+#endif
 
 		/* update model */
 		inc_freq((uchar)c);
+
+#ifdef ADAPTIVE
+		update_model(d);
+#endif
 	} while (1);
 }
 
@@ -146,7 +191,9 @@ int main(int argc, char *argv[])
 	FILE *ostream = fopen(argc > 2 ? argv[2] : "L.gr", "w");
 	struct bio bio;
 	void *ptr = malloc(100000000);
+#ifndef ADAPTIVE
 	struct vector vector;
+#endif
 
 	if (istream == NULL) {
 		abort();
@@ -162,15 +209,24 @@ int main(int argc, char *argv[])
 
 	init();
 
+#ifndef ADAPTIVE
 	vector_init(&vector, 100000000);
+#endif
 
 	bio_open(&bio, ptr, BIO_MODE_WRITE);
+#ifdef ADAPTIVE
+	process(istream, &bio);
+	printf("opt. k = %lu\n", (unsigned long)opt_k);
+#else
 	process(istream, &vector);
 	vector_to_bio(&vector, &bio);
+#endif
 	bio_close(&bio);
 	bio_dump(&bio, ptr, ostream);
 
+#ifndef ADAPTIVE
 	vector_free(&vector);
+#endif
 
 	fclose(istream);
 	fclose(ostream);
